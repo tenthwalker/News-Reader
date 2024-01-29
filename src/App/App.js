2beb9ec957c43c902ed2174f284923b16aae850b
Arden Ranta <arranta20@gmail.com>
2024-01-28 21:15:31 -0800

    feat/add sort functionality

--- src/App/App.js
+++ src/App/App.js
@@ -5,6 +5,7 @@ import Main from '../Main/Main.js'
 
 function App() {
   const [isExp, setExp] = useState([]);
+  const [isChrono, setChrono] = useState(true);
   const [articles, setArticles] = useState([]);
   const url = 'https://newsapi.org/v2/top-headlines?';
 
@@ -14,8 +15,29 @@ function App() {
     setArticles(response)
   }  
 
+  function toggleSort() {
+    console.log("toggle")
+    console.log(isChrono)
+    console.log(articles, "inside toggle articles")
+    if (isChrono) {
+      setChrono(false)
+      const trueTime = articles.sort((a, b)=> {
+        return new Date(a.publishedAt)- new Date(b.publishedAt)
+      })
+      console.log(trueTime, "trueTime")
+      setArticles(trueTime)
+    } else {
+      setChrono(true)
+      const reverseTime = articles.sort((a, b)=> {
+        return new Date(b.publishedAt)- new Date(a.publishedAt)
+      })
+      console.log(reverseTime, "reverseTime")
+      setArticles(reverseTime)
+    }
+    return articles
+  }
+
   function toggleExp(selectedArt) {
-    console.log(selectedArt, "inside toggle, selected Article")
     const isInList = isExp.find(article => article.title === selectedArt.title)
     isInList ? handleCollapse(selectedArt) : handleExpand(selectedArt);
     return isInList
@@ -54,6 +76,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>News Reader</h1>
+        <button onClick={() => toggleSort()}>{isChrono ? "sort oldest to newest" : "sort newest to oldest"}</button>
       </header>
       <main>
         <Main articles={articles} isExp={isExp} toggleExp={toggleExp}/>
