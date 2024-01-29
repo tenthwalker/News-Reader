0e62c973b114a6c34b22605927ffacfcc0efb99d
Arden Ranta <arranta20@gmail.com>
2024-01-28 17:20:05 -0800

    add error page component

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,14 @@
 import { v4 as uuid } from "uuid";
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NotFound from '../Error/Error.js';
 import './App.css';
 
 function App() {
   const newId = uuid();
   const abbId = newId.slice(0, 8);
 
-  
+
   return (
     <div className="App">
       <header className="App-header">
