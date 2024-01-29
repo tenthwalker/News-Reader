2d2a944f3a2e6c4716673038cc5796c3042de4a6
Arden Ranta <arranta20@gmail.com>
2024-01-28 17:07:10 -0800

    add mock data for API calls placeholder

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,11 @@
-
+import { v4 as uuid } from "uuid";
 import './App.css';
 
 function App() {
+  const newId = uuid();
+  const abbId = newId.slice(0, 8);
+
+  
   return (
     <div className="App">
       <header className="App-header">
